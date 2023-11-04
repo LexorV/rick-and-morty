131119ce1b5f39b689972b63b5d1f2a79cba833b
Lex <Lexor.V.A.S@yandex.ru>
2023-11-04 19:31:14 +0300

    add header&route&scss

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <img class="logo" src="@/assets/logo.png">
+    <h1>Заголовок</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMenu'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  max-width: $desktop ;
+}
+.logo {
+  width: 100px;
+  height: 50px;
+}
+h1 {
+  margin: 40px 0 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router'
+import {
+    createPinia
+} from "pinia";
+import MainPage from '@/page/MainPage'
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+    routes: [{
+        path: '/',
+        component: MainPage
+    }, ],
+    history: createWebHistory()
+})
+
+
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .use(VueAxios, axios)
+    .mount('#app')
